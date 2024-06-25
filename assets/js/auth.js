073625eac38cf018cc740a688cf34fe92437ff63
MarcioAlex-x <marcioalex586@gmail.com>
2024-06-25 16:29:54 -0300

    Login com email e senha no firebase

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -1,52 +1,27 @@
 import { auth } from './firebase.js'
-
-import { signInWithEmailAndPassword, onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js'
+import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js"
 
 document.addEventListener('DOMContentLoaded', () => {
+    const formLogin = document.querySelector('.form-login')
     const emailInput = document.querySelector('.email-admin')
     const senhaInput = document.querySelector('.senha-admin')
-    const formLogin = document.querySelector('.form-login')
-    const linkSair = document.querySelector('.link-sair')
-    const linkAdmin = document.querySelector('.link-admin')
-    const formPost = document.querySelector('.form-post')
-    const tituloLogin = document.querySelector('.titulo-login')
 
-    if(formLogin){
-     formLogin.addEventListener('submit', (e) => {
+    formLogin.addEventListener('submit', (e) => {
         e.preventDefault()
         const email = emailInput.value
-        const password = senhaInput.value
-        signInWithEmailAndPassword(auth, email, password)
-            .then((userCredential) => {
+        const senha = senhaInput.value
 
+        if(formLogin){
+            signInWithEmailAndPassword(auth, email, senha)
+            .then((userCredential) => {
                 const user = userCredential.user
+                alert('Usuário logado com sucesso')
                 emailInput.value = ''
                 senhaInput.value = ''
-
             })
-            .catch((error)=>alert(error.message))
-    })   
-    }
-
-    onAuthStateChanged(auth,(user)=>{
-        if(user){
-            const uid = user.uid
-            linkAdmin.classList.add('hide')
-            linkSair.classList.remove('hide')
-            formLogin.classList.add('hide')
-            formPost.classList.remove('hide')
-            tituloLogin.innerHTML = 'ACESSO AO ADMINISTRADOR LIBERADO'
-
-        }else{
-            linkAdmin.classList.remove('hide')
-            linkSair.classList.add('hide')
-            formLogin.classList.remove('hide')
-            formPost.classList.add('hide')
-            tituloLogin.innerHTML = 'FAÇA O LOGIN COMO ADMINISTRADOR'
-
+            .catch(()=>{
+                alert('Verifique a sua conexão ou o seu email e senha.')
+            })
         }
     })
-
 })
-
-
--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -1,9 +1,11 @@
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js"
 
 const firebaseConfig = {
   apiKey: "AIzaSyDiIVLic_s18atZtBaJmTSNDVjqO_7u1FU",
   authDomain: "turmaterca-d2878.firebaseapp.com",
+  databaseURL: "https://turmaterca-d2878-default-rtdb.firebaseio.com",
   projectId: "turmaterca-d2878",
   storageBucket: "turmaterca-d2878.appspot.com",
   messagingSenderId: "179178679657",
