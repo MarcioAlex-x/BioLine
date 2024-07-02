3722622a3af41d6ce7b1ed341faaffb75a3cf8cf
MarcioAlex-x <marcioalex586@gmail.com>
2024-07-02 16:34:58 -0300

    concluiu login, logout e estado de autenticidade

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -1,27 +1,83 @@
 import { auth } from './firebase.js'
-import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js"
+import { signInWithEmailAndPassword, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js"
 
 document.addEventListener('DOMContentLoaded', () => {
     const formLogin = document.querySelector('.form-login')
     const emailInput = document.querySelector('.email-admin')
     const senhaInput = document.querySelector('.senha-admin')
+    const linkSair = document.querySelector('.link-sair')
+    const formPost = document.querySelector('.form-post')
+    const tituloLogin = document.querySelector('.titulo-login')
+    const mensagens = document.querySelector('.mensagens')
 
-    formLogin.addEventListener('submit', (e) => {
-        e.preventDefault()
-        const email = emailInput.value
-        const senha = senhaInput.value
+    // mensagens
+    const logadoSucesso = () =>{
+        mensagens.innerHTML = 'Usuário logado com sucesso.'
+    }
 
-        if(formLogin){
+    const verifiqueConexao = () =>{
+        mensagens.innerHTML = 'Verifique a sua conexão ou o seu email e senha.'
+    }
+
+    const logoutSucesso = () =>{
+        mensagens.innerHTML = 'Logout realizado com sucesso.'
+    }
+
+    const erroInesperado = () =>{
+        mensagens.innerHTML = 'Ocorreu um erro inesperado.'
+    }
+
+
+
+    // Evento de login
+    if (formLogin) {
+        formLogin.addEventListener('submit', (e) => {
+            e.preventDefault()
+            const email = emailInput.value
+            const senha = senhaInput.value
+
             signInWithEmailAndPassword(auth, email, senha)
-            .then((userCredential) => {
-                const user = userCredential.user
-                alert('Usuário logado com sucesso')
-                emailInput.value = ''
-                senhaInput.value = ''
-            })
-            .catch(()=>{
-                alert('Verifique a sua conexão ou o seu email e senha.')
-            })
+                .then((userCredential) => {
+                    const user = userCredential.user
+                    logadoSucesso()
+                    emailInput.value = ''
+                    senhaInput.value = ''
+                })
+                .catch(() => {
+                    verifiqueConexao()
+                })
+        })
+    }
+
+    // Evento de logout
+    if (linkSair) {
+        linkSair.addEventListener('click', () => {
+            signOut(auth)
+                .then(()=>{
+                    logoutSucesso()
+                })
+                .catch(()=>{
+                    erroInesperado()
+                })
+        })
+    }
+
+    // Mudança de estado
+    onAuthStateChanged(auth,(user)=>{
+        if(user){
+            const uid = user.uid
+
+            if(linkSair) linkSair.classList.remove('hide')
+            if(formLogin) formLogin.classList.add('hide')
+            if(formPost) formPost.classList.remove('hide')
+            if(tituloLogin) tituloLogin.innerHTML = 'ACESSO AO ADMINISTRADOR LIBERADO'
+        }else{
+            if(linkSair) linkSair.classList.add('hide')
+            if(formLogin) formLogin.classList.remove('hide')
+            if(formPost) formPost.classList.add('hide')
+            if(tituloLogin) tituloLogin.innerHTML = 'FAÇA O LOGIN COMO ADMINISTRADOR!'
         }
     })
+
+
 })
